bb2a70bf075ffc6e0d6ddde926f0e9079c88233a
kikker234 <jusraaijmakers@gmail.com>
2025-06-03 14:10:37 +0200

    Bugfix

--- Loading/Reader/FsmFileReader.cs
+++ Loading/Reader/FsmFileReader.cs
@@ -5,7 +5,7 @@ public class FsmFileReader : IFileReader
     public List<string> ReadFile(string fileContent)
     {
         return fileContent
-            .Split("\r\n", StringSplitOptions.RemoveEmptyEntries)
+            .Split(new[] { "\r\n", "\n", "\r" }, StringSplitOptions.RemoveEmptyEntries)
             .Where(line => !line.StartsWith("#"))
             .ToList();
     }
